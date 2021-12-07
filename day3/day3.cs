419f012eedfb7baca46640fd7e2f1319b5a3f2d2
Peccat <80050651+Peccat@users.noreply.github.com>
2021-12-07 22:32:53 +0100

    day3 part2?

--- day3/day3.cs
+++ day3/day3.cs
@@ -10,21 +10,23 @@ public class day3{
             data.Add(oneline);
         }
 
-        
-        Console.WriteLine("Part1: " + Part1(data));
-        Console.WriteLine("Part1: " + Part2(data));
-    }
-
-    public static int Part1(List<string> datas){
-        int[,] BinaryArray = new int[datas.Count,((datas[0].ToCharArray()).Length)];
-        for(int j = 0; j < datas.Count; j++){
-            char[] line = datas[j].ToCharArray();
+        int[,] BinaryArray = new int[data.Count,((data[0].ToCharArray()).Length)];
+        for(int j = 0; j < data.Count; j++){
+            char[] line = data[j].ToCharArray();
             for(int i = 0; i < line.Length; i++){
                 BinaryArray[j,i] = line[i]-48;
                 //Console.Write(BinaryArray[j,i]);
             }
             //Console.WriteLine();
         }
+
+        
+        Console.WriteLine("Part1: " + Part1(BinaryArray));
+        Console.WriteLine("Part2: " + Part2(data));
+    }
+
+    public static int Part1(int[,] BinaryArray){
+        
 
         int ones = 0;
         string gamma = "";
@@ -54,8 +56,32 @@ public class day3{
 
         return GetDecimal(gamma) * GetDecimal(epsilon);
     }
-    public static int Part2(List <string> datas){
-       return 0;
+    public static int Part2( List<string> datas){
+        int ones = 0;
+
+        for(int i = 0; i < 12;i++){ //oszlopok száma
+            for(int j = 0; j < datas.Count; j++){
+                if(datas[j][i] == '1'){
+                    ones++;
+                }
+            }
+            Console.WriteLine(ones);
+            if(ones > datas.Count-1){
+                for(int j = datas.Count-1; j >= 0; j--){
+                    if(datas[j][i] == '1'){
+                        datas.RemoveAt(j);
+                    }
+                }
+            }else{
+                for(int j = datas.Count-1; j >= 0; j--){
+                    if(datas[j][i] == '0'){
+                        datas.RemoveAt(j);
+                    }
+                }
+            ones = 0;
+            }
+        }
+       return datas.Count;
     }
 
     public static int GetDecimal(string binaryNumber){
